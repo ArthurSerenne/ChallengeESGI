72340a418671d9ab14177292f7851e241869310f
Arthur serenne <arthur.serenne@gmail.com>
2024-07-01 16:33:57 +0200

    Ajout template Entreprise

--- src/Form/CompanyFormType.php
+++ src/Form/CompanyFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Company;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CompanyFormType extends AbstractType
@@ -12,8 +13,12 @@ class CompanyFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('siret')
+            ->add('name', TextType::class, [
+                'label' => 'Nom de l\'entreprise',
+            ])
+            ->add('siret', TextType::class, [
+                'label' => 'Siret de l\'entreprise',
+            ])
         ;
     }
 
