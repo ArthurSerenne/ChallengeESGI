1eee0d38b5a70cb2e077a3947f105f53f1bc00e8
Arthur serenne <arthur.serenne@gmail.com>
2024-07-01 23:22:47 +0200

    Modification vue CRUD user pour l'admin

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Repository\UserRepository;
+use App\Form\UserFormType;
+use Doctrine\ORM\EntityManagerInterface;
 
 class UserController extends AbstractController
 {
@@ -24,101 +27,67 @@ class UserController extends AbstractController
             'theme' => $theme,
         ]);
     }
-    
 
-    
-    #[Route('dashboard/clients/new', name: 'app_user_create')]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $user = $this->getUser();
-        $company = $user->getCompany();
-        $theme = $user ? $user->getTheme() : 'original';
-
-        $client = new Clients();
-
-        $client->setCreatedAt(new \DateTime());
-        $form = $this->createForm(ClientsFormType::class, $client);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $client->setCompany($company);
-            $entityManager->persist($client);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_clients');
-        }
-
-        return $this->render('backoffice/clients/create.html.twig', [
-            'clientForm' => $form->createView(),
-            'company' => $company,
-            'theme' => $theme,
-        ]);
-    }
-
-    #[Route('dashboard/clients/{id}', name: 'app_user_view')]
+    #[Route('dashboard/user/{id}', name: 'app_user_view')]
     public function view(int $id, EntityManagerInterface $entityManager): Response
     {
         $user = $this->getUser();
-        $company = $user->getCompany();
         $theme = $user ? $user->getTheme() : 'original';
         
-        $client = $entityManager->getRepository(Clients::class)->find($id);
+        $user = $entityManager->getRepository(User::class)->find($id);
 
-        if (!$client) {
-            throw $this->createNotFoundException('Aucun client trouvé !!');
+        if (!$user) {
+            throw $this->createNotFoundException('Aucun utilisateur trouvé !!');
         }
 
-        return $this->render('backoffice/clients/view.html.twig', [
-            'client' => $client,
-            'company' => $company,
+        return $this->render('backoffice/users/view.html.twig', [
+            'user' => $user,
             'theme' => $theme,
         ]);
     }
 
-    #[Route('dashboard/clients/{id}/edit', name: 'app_user_edit')]
+    #[Route('dashboard/user/{id}/edit', name: 'app_user_edit')]
     public function edit(int $id, Request $request, EntityManagerInterface $entityManager): Response
     {
         $user = $this->getUser();
-        $company = $user->getCompany();
         $theme = $user ? $user->getTheme() : 'original';
         
-        $client = $entityManager->getRepository(Clients::class)->find($id);
+        $user = $entityManager->getRepository(User::class)->find($id);
 
-        if (!$client) {
-            throw $this->createNotFoundException('Client not found');
+        if (!$user) {
+            throw $this->createNotFoundException('Aucun utilisateur trouvé !!');
         }
 
-        $form = $this->createForm(ClientsFormType::class, $client);
+        $form = $this->createForm(UserFormType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_clients'); // Assurez-vous d'avoir une route vers la liste des clients
+            return $this->redirectToRoute('app_user'); // Assurez-vous d'avoir une route vers la liste des clients
         }
 
-        return $this->render('backoffice/clients/edit.html.twig', [
-            'clientForm' => $form->createView(),
-            'company' => $company,
+        return $this->render('backoffice/users/edit.html.twig', [
+            'userForm' => $form->createView(),
             'theme' => $theme,
         ]);
     }
 
-    #[Route('dashboard/clients/{id}/delete', name: 'app_user_delete', methods: ['POST'])]
-    public function delete(int $id, EntityManagerInterface $entityManager, Request $request, Clients $client): Response
+    #[Route('dashboard/user/{id}/delete', name: 'app_user_delete', methods: ['POST'])]
+    public function delete(int $id, EntityManagerInterface $entityManager, Request $request, User $client): Response
     {
         if ($this->isCsrfTokenValid('delete'.$client->getId(), $request->request->get('_token'))) {
             try {
                 $entityManager->remove($client);
                 $entityManager->flush();
-                $this->addFlash('success', 'Le client a été supprimé avec succès.');
+                $this->addFlash('success', 'L\'utilisateur a été supprimé avec succès.');
             } catch (\Exception $e) {
-                $this->addFlash('error', 'Impossible de supprimer ce client car il est rataché à un/plusieurs devis.');
+                $this->addFlash('error', 'Impossible de supprimer cet utilisateur');
             }
         } else {
             $this->addFlash('error', 'Token CSRF invalide.');
         }
 
-        return $this->redirectToRoute('app_clients');
+        return $this->redirectToRoute('app_user');
     }
 }
